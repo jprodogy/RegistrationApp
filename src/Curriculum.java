4acfdbdcfd156fb2f29565a1afb4d42a47d01ca6
Jeffrey Boudreaux <jaboudreauxjr@gmail.com>
2018-03-05 20:06:15 -0600

    There is a slight problem with my method

--- src/Curriculum.java
+++ src/Curriculum.java
@@ -1,44 +1,34 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Curriculum{
-    private ArrayList<String> classType;
-    private ArrayList<String> classId;
-    private ArrayList<String> credits;
+    private Map<StringBuilder, Integer> curMap;
     static String fn = "curriculum.dat";
 
     public Curriculum(){
-        
+
     }
 
     //read and store data in a curriculum
     public void CurriculumFile() throws FileNotFoundException {
         Scanner curriculumScan = new Scanner(new File(fn));
         while(curriculumScan.hasNextLine()) {
-            String info = curriculumScan.next();
-            if (info == "*"){
-                System.out.println("d");
-            }else {
-                int num = curriculumScan.nextInt();
-                int num2 = curriculumScan.nextInt();
-                System.out.println(info + num + num2);
+            StringBuilder str = new StringBuilder();
+            String[] sArray = curriculumScan.nextLine().split(" ");
+            int classCre = Integer.parseInt(sArray[sArray.length - 1]);
+            for (int i = 0; i < sArray.length - 1; i++) {
+                str.append(sArray[i]);
             }
-
+            System.out.println(str);
+            curMap.put(str, classCre);
         }
     }
 
-    @Override
-    public String toString() {
-        return "Curriculum{" +
-                "classType=" + classType +
-                ", classId=" + classId +
-                ", credits=" + credits +
-                '}';
+    public void curriulumEdit(){
+
     }
-
-
 
 
 }
